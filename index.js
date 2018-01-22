df25e82859b48e7d67ff25fa1bed12905d920a5d
Harikrishnan V <39754@hexaware.com>
2018-01-22 12:16:28 +0530

    commited in bracnch

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const app = express()
+const bodyparser = require('body-parser')
+
+
+app.use(bodyparser.json());
+app.post('/add', (req, res) => { 
+var result = req.body.data1+req.body.data2;
+res.send('Addition :'+result);
+console.log(req.body)
+});
+app.post('/divide', (req, res) => { 
+var result = req.body.data1/req.body.data2;
+res.setHeader('Content-Type', 'application/json');
+res.send(JSON.stringify({ Division:result }));
+console.log(req.body)
+});
+
+app.get('/multiply/', (req, res) => { 
+var result = req.query.data1*req.query.data2;
+res.send('Product :'+result);
+console.log(req.query)
+});
+app.get('/hi', (req, res) => res.send('Hello There!'))
+
+
+
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
