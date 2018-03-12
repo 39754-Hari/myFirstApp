6448e2dd5c015249a14a81c02c66947c268ea12f
Harikrishnan V <39754@hexaware.com>
2018-03-12 12:31:32 +0530

    Log files

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ const express = require('express')
 const app = express()
 const bodyparser = require('body-parser')
 const { DialogflowApp } = require('actions-on-google');
+const logger = require('nodejslogger')
+logger.init({"file":"./logs/logFile.log", "mode":"DIE"})
 app.use(bodyparser.json());
 app.use(express.static('html'));
 app.post('/welcome', (req, res) =>{ 
   console.log('initial req:',req.body.result.resolvedQuery);
+  logger.info('initial req:',req.body.result.resolvedQuery);
   /*if(req.body.originalRequest.source === 'facebook'){
     facebook.operation(req,res);
   }
