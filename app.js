304f7220d1640261fbe40ad80aac1c440c9d9bf0
Harikrishnan V <39754@hexaware.com>
2019-09-10 11:13:08 +0530

    test

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 const bodyparser = require('body-parser')
 const { DialogflowApp } = require('actions-on-google');
 const logger = require('nodejslogger')
-logger.init({"file":"./logs/logFile.log", "mode":"DIE"})
+//logger.init({"file":"./logs/logFile.log", "mode":"DIE"})
 app.use(bodyparser.json());
 app.use(express.static('html'));
 app.post('/welcome', (req, res) =>{ 
